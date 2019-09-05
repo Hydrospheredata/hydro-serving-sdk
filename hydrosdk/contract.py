1a01f87b83bba4ed87ae0f6342afa22d3ace38f4
Yuriy Gavrilin <snyderino@gmail.com>
2019-09-05 13:02:58 +0300

    updates on contract

--- hydrosdk/contract.py
+++ hydrosdk/contract.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from typing import Tuple, List
 
+import numpy as np
 from hydro_serving_grpc.contract import ModelContract, ModelSignature, ModelField
 
 from .proto_conversion_utils import np2proto_shape, np2proto_dtype, proto2np_shape, proto2np_dtype
@@ -51,6 +52,49 @@ class Field:
                    dtype=proto2np_dtype(proto.dtype),
                    profile=proto.profile)
 
+    def validate_shape(self, other_shape: Tuple[int]):
+        if len(self.shape) == 0:
+            # scalar input can be used in following scenarios
+            if other_shape == tuple():
+                return True, None
+            else:
+                if max(other_shape) == 1:  # All dimensions are equal to 1
+                    return True, None
+                else:
+                    return False, "Tensor {} has scalar shape. Recieved tensor is of shape {}".format(self.name, other_shape)
+
+        if len(self.shape) == len(other_shape):
+            possible_shape = tuple([AnyDimSize if s == -1 else s for s in self.shape])
+            is_valid = possible_shape == other_shape
+        else:
+            is_valid = False
+
+        if not is_valid:
+            return False, "Tensor {} has invalid other_shape. Expected {}, received {}".format(self.name, self.shape, other_shape)
+        else:
+            return True, None
+
+    def validate_dtype(self, other_dtype):
+        # TODO Use np.can_cast rules? i.e. float32 can be casted to float16
+        #  add strict = True for strict matching and strict = False for supporting subtypes?
+
+        if self.dtype == other_dtype:
+            return True, None
+        else:
+            return False, "Tensor {} has invalid dtype. Expected {}, recieved {}".format(self.name, self.dtype, other_dtype)
+
+    def validate(self, x: np.array):
+        """
+        Return bool whether array is valid for this field and error message, if not valid.
+        Error message is None if array is valid.
+        :param x: input ndarray
+        :return: is_valid, error_message
+        """
+        is_shape_valid, shape_error_message = self.validate_shape(x.shape)
+        is_dtype_valid, dtype_error_message = self.validate_dtype(x.dtype)
+        error_message = ', '.join(filter(None, (shape_error_message, dtype_error_message)))
+        return is_dtype_valid & is_dtype_valid, error_message if error_message else None
+
 
 class Signature:
 
@@ -71,6 +115,58 @@ class Signature:
                    inputs=[Field.from_proto(p) for p in proto.inputs],
                    outputs=[Field.from_proto(p) for p in proto.outputs])
 
+    @staticmethod
+    def __validate_dict(tensor_dict, fields):
+        is_valid = True
+        error_messages = []
+
+        extra_tensor_names = set(tensor_dict.keys()).difference(set(map(lambda x: x.name, fields)))
+        missing_tensor_names = set(map(lambda x: x.name, fields)).difference(set(tensor_dict.keys()))
+        common_tensor_names = set(tensor_dict.keys()).intersection(set(map(lambda x: x.name, fields)))
+
+        common_tensor_names = list(common_tensor_names)
+        common_fields = tensor_dict(
+            zip(common_tensor_names, [next(filter(lambda x: x.name == name, fields)).val for name in common_tensor_names]))
+
+        if extra_tensor_names:
+            is_valid = False
+        error_messages.append("Extra tensors provided: {}".format(extra_tensor_names))
+
+        if missing_tensor_names:
+            is_valid = False
+        error_messages.append("Missing tensors: {}".format(missing_tensor_names))
+
+        for tensor_name in common_tensor_names:
+            is_tensor_valid, error_message = common_fields[tensor_name].validate(tensor_dict[tensor_name])
+            is_valid &= is_tensor_valid
+            if error_message:
+                error_messages.append(error_message)
+
+        return is_valid, error_messages
+
+    def validate_input(self, input_dict):
+        return self.__validate_dict(input_dict, self.inputs)
+
+    def validate_output(self, output_dict):
+        return self.__validate_dict(output_dict, self.outputs)
+
+    def merge_sequential(self, other_signature):
+        if set(self.outputs) != set(other_signature.inputs):
+            raise ContractViolationException("Invalid blablalba")
+
+        return Signature("|".join([self.name, other_signature.name]),
+                         self.inputs,
+                         other_signature.outputs)
+
+    def mock_input_data(self):
+        # work in progress
+        input_dict = {}
+        for field in self.inputs:
+            x = (10 * np.random.randn(*field.shape)).astype(field.dtype)
+            input_dict[field.name] = x
+
+        return input_dict
+
 
 class Contract:
 
@@ -86,13 +182,13 @@ class Contract:
         pass
 
     def merge_sequential(self, other_contract):
-        pass
+        return Contract(self.model_name, self.signature.merge_sequential(other_contract.signature))
 
     def merge_parallel(self, other_contract):
         pass
 
     def mock_data(self):
-        pass
+        return self.signature.mock_input_data()
 
     @property
     def proto(self):
