e6159c1b21f42ad68c74509152c73ceb03cbbd1b
Bulat Lutfullin <lb6557@gmail.com>
2020-02-06 12:55:46 +0300

    cleanup mock tests

--- tests/test_mock.py
+++ tests/test_mock.py
@@ -14,29 +14,25 @@ SUPPORTED_NP_TYPES = [np.int, np.int64, np.int32, np.int16, np.int8, np.int0,
 UNSUPPORTED_NP_TYPES = [np.float128, np.complex256]
 
 
-def test_tensor_data():
-    for t in SUPPORTED_NP_TYPES:
-        dtype = np.dtype(t)
-        print("Mocking {} dtype".format(dtype))
+@pytest.mark.parametrize('np_type', SUPPORTED_NP_TYPES)
+def test_tensor_data(np_type):
+    s = SignatureBuilder("change_state") \
+        .with_input("tensor1", np_type, hs.scalar) \
+        .with_input("tensor2", np_type, [-1, 10]) \
+        .build()
+
+    mock_data = mock_input_data(s)
+    assert mock_data is not None
+    # is_valid, error_msg = s.validate_input(mock_data)
+    # self.assertTrue(is_valid)
+    # self.assertIsNone(error_msg)
+
+
+@pytest.mark.parametrize('np_type', UNSUPPORTED_NP_TYPES)
+def test_unsupported(np_type):
+    with pytest.raises(KeyError):
         s = SignatureBuilder("change_state") \
-            .with_input("tensor1", t, hs.scalar) \
-            .with_input("tensor2", t, [-1, 10]) \
+            .with_input("tensor1", np_type, hs.scalar) \
+            .with_input("tensor2", np_type, [-1, 10]) \
             .build()
-
-        mock_data = mock_input_data(s)
-        assert mock_data is not None
-        # is_valid, error_msg = s.validate_input(mock_data)
-        # self.assertTrue(is_valid)
-        # self.assertIsNone(error_msg)
-
-
-def test_unsupported():
-    for t in UNSUPPORTED_NP_TYPES:
-        dtype = np.dtype(t)
-        print("Trying to mock {} dtype".format(dtype))
-        with pytest.raises(KeyError):
-            s = SignatureBuilder("change_state") \
-                .with_input("tensor1", t, hs.scalar) \
-                .with_input("tensor2", t, [-1, 10]) \
-                .build()
-            mock_input_data(s)
+        mock_input_data(s)
