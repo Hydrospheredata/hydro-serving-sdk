615258d902333c216541ca08354842d4f97bf9d1
Bulat Lutfullin <lb6557@gmail.com>
2020-03-19 18:35:26 +0300

    remove venv for now

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,27 +1,25 @@
 def repository = 'hydro-serving-sdk'
 
 def buildAndPublishReleaseFunction={
-    sh "sudo apt-get install python3-venv"
-    sh "python3 -m venv venv"
-    sh "source venv/bin/activate"
-    sh "pip install -r requirements.txt"
+//   need  python3-venv on Jenkins slave
+//    sh "python3 -m venv venv"
+//    sh "source venv/bin/activate"
+    sh "sudo pip install -r requirements.txt"
     sh "python setup.py bdist_wheel"
 //    sh "pytest"   //NB(bulat): commented because there are no mocks for tests
     configFileProvider([configFile(fileId: 'PYPIDeployConfiguration', targetLocation: '.pypirc', variable: 'PYPI_SETTINGS')]) {
         sh "twine upload --config-file ${env.WORKSPACE}/.pypirc -r pypi ${env.WORKSPACE}/dist/*"
     }
-    sh "deactivate"
+//    sh "deactivate"
 }
 
 def buildFunction={
-    sh "sudo apt-get install python3-venv"
-    sh "python3 -m venv venv"
-    sh "source venv/bin/activate"
-    sh "pip install -r requirements.txt"
+//    sh "python3 -m venv venv"
+//    sh "source venv/bin/activate"
+    sh "sudo pip install -r requirements.txt"
     sh "python setup.py bdist_wheel"
     //    sh "pytest"   //NB(bulat): commented because there are no mocks for tests
-
-    sh "deactivate"
+//    sh "deactivate"
 }
 
 def collectTestResults = {
