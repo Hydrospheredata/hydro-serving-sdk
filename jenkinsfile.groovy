756b5cd0607dbd1b7da14b9576bc2502c281f3c4
Bulat <lb6557@gmail.com>
2020-03-20 22:16:44 +0200

    jenkinsfile (#26)
    * Jenkinsfile
    
    * add python3-venv
    
    * remove venv for now
    
    * remove sudo
    
    * restart job
    
    * change executables
    
    * add version
    
    * ignore egg-info
    
    * update version

---
+++ jenkinsfile.groovy
@@ -0,0 +1,37 @@
+def repository = 'hydro-serving-sdk'
+
+def buildAndPublishReleaseFunction={
+//   need  python3-venv on Jenkins slave
+//    sh "python3 -m venv venv"
+//    sh "source venv/bin/activate"
+    sh "pip3 install -r requirements.txt"
+    sh "python3 setup.py bdist_wheel"
+//    sh "pytest"   //NB(bulat): commented because there are no mocks for tests
+    configFileProvider([configFile(fileId: 'PYPIDeployConfiguration', targetLocation: '.pypirc', variable: 'PYPI_SETTINGS')]) {
+        sh "twine upload --config-file ${env.WORKSPACE}/.pypirc -r pypi ${env.WORKSPACE}/dist/*"
+    }
+//    sh "deactivate"
+}
+
+def buildFunction={
+//    sh "python3 -m venv venv"
+//    sh "source venv/bin/activate"
+    sh "pip3 install -r requirements.txt"
+    sh "python3 setup.py bdist_wheel"
+    //    sh "pytest"   //NB(bulat): commented because there are no mocks for tests
+//    sh "deactivate"
+}
+
+def collectTestResults = {
+    junit testResults: 'test-report.xml', allowEmptyResults: true
+}
+
+pipelineCommon(
+    repository,
+    false, //needSonarQualityGate,
+    [],
+    collectTestResults,
+    buildAndPublishReleaseFunction,
+    buildFunction,
+    buildFunction
+)
