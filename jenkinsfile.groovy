ed947ecca7134ebdcccc2e1aa37e8aa9700ded0c
Bulat Lutfullin <lb6557@gmail.com>
2020-03-26 14:04:13 +0300

    update release job

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,19 +1,20 @@
 def repository = 'hydro-serving-sdk'
 
-def buildAndPublishReleaseFunction={
-//   need  python3-venv on Jenkins slave
-//    sh "python3 -m venv venv"
-//    sh "source venv/bin/activate"
-    sh "pip3 install -r requirements.txt"
-    sh "python3 setup.py bdist_wheel"
-//    sh "pytest"   //NB(bulat): commented because there are no mocks for tests
+def buildAndPublishReleaseFunction = {
     configFileProvider([configFile(fileId: 'PYPIDeployConfiguration', targetLocation: '.pypirc', variable: 'PYPI_SETTINGS')]) {
-        sh "python3 -m twine upload --config-file ${env.WORKSPACE}/.pypirc -r pypi ${env.WORKSPACE}/dist/*"
+        sh """#!/bin/bash
+        python3 -m venv venv
+        source venv/bin/activate
+        pip install wheel~=0.34.2
+        pip install -r requirements.txt
+        python setup.py bdist_wheel
+        #pytest
+        python -m twine upload --config-file ${env.WORKSPACE}/.pypirc -r pypi ${env.WORKSPACE}/dist/*
+    """
     }
-//    sh "deactivate"
 }
 
-def buildFunction={
+def buildFunction = {
     sh """#!/bin/bash
         python3 -m venv venv
         source venv/bin/activate
