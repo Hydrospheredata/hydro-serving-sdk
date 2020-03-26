edc69425e9372b12eb218c4b999ece507203d075
Bulat <lb6557@gmail.com>
2020-03-26 12:22:39 +0300

    Chore/venv (#29)
    * enable venv for build
    
    * fix (possible) for source not found error
    
    * add bash shebang
    
    * add wheel to deps
    
    * add preemptive wheel to fix errors during pyyaml installation

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -14,12 +14,15 @@ def buildAndPublishReleaseFunction={
 }
 
 def buildFunction={
-//    sh "python3 -m venv venv"
-//    sh "source venv/bin/activate"
-    sh "pip3 install -r requirements.txt"
-    sh "python3 setup.py bdist_wheel"
-    //    sh "pytest"   //NB(bulat): commented because there are no mocks for tests
-//    sh "deactivate"
+    sh """#!/bin/bash
+        python3 -m venv venv
+        source venv/bin/activate
+        pip install wheel~=0.34.2
+        pip install -r requirements.txt
+        python setup.py bdist_wheel
+        #pytest
+        deactivate
+    """
 }
 
 def collectTestResults = {
