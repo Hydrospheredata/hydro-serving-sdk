095f93c1201029ee21645ab3263e5d573ed93823
techkuz <k.leshakov@yandex.ru>
2020-04-06 08:50:14 +0300

    Add create_test_app

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,23 +1,27 @@
 import os
 import time
+
 import pytest
 import yaml
 
 from hydrosdk.application import Application
 from tests.test_model import get_cluster, get_local_model
 
-
 DEFAULT_APP_NAME = "test-app"
 
 
-def create_test_application(cluster, upload_response, model):
+def create_test_application(cluster):
+    local_model = get_local_model()
+    upload_response = local_model.upload(cluster=cluster)
+
     with open(os.path.dirname(os.path.abspath(__file__)) + '/resources/application.yml') as f:
         d = yaml.safe_load(f)
         app = Application.parse_application(d)
         app_as_dict = app._asdict()
-        app_as_dict["executionGraph"]["stages"][0]["modelVariants"][0]["modelVersionId"] = upload_response[model].model_version_id
+        app_as_dict["executionGraph"]["stages"][0]["modelVariants"][0]["modelVersionId"] = upload_response[
+            local_model].model_version_id
 
-        while upload_response[model].building():
+        while upload_response[local_model].building():
             print("building")
 
         time.sleep(3)
@@ -28,10 +32,8 @@ def create_test_application(cluster, upload_response, model):
 
 def test_list_all():
     cluster = get_cluster()
-    local_model = get_local_model()
-    upload_response = local_model.upload(cluster=cluster)
 
-    created_application = create_test_application(cluster=cluster, model=local_model, upload_response=upload_response)
+    created_application = create_test_application(cluster=cluster)
     all_applications = Application.list_all(cluster)
 
     assert all_applications is not None
@@ -39,21 +41,16 @@ def test_list_all():
 
 def test_find_by_name():
     cluster = get_cluster()
-    local_model = get_local_model()
-    upload_response = local_model.upload(cluster=cluster)
 
-    created_application = create_test_application(cluster=cluster, model=local_model, upload_response=upload_response)
+    created_application = create_test_application(cluster)
     found_application = Application.find_by_name(cluster=cluster, app_name=DEFAULT_APP_NAME)
     assert found_application.name == DEFAULT_APP_NAME
 
 
 def test_delete():
     cluster = get_cluster()
-    local_model = get_local_model()
-    upload_response = local_model.upload(cluster=cluster)
 
-    created_application = create_test_application(cluster=cluster, model=local_model,
-                                                  upload_response=upload_response)
+    created_application = create_test_application(cluster=cluster)
     deleted_application = Application.delete(cluster=cluster, app_name=DEFAULT_APP_NAME)
 
     with pytest.raises(Exception, match=r"Failed to find by name.*"):
@@ -62,10 +59,8 @@ def test_delete():
 
 def test_create():
     cluster = get_cluster()
-    local_model = get_local_model()
-    upload_response = local_model.upload(cluster=cluster)
 
-    created_application = create_test_application(cluster=cluster, model=local_model, upload_response=upload_response)
+    created_application = create_test_application(cluster=cluster)
 
     all_applications = Application.list_all(cluster=cluster)
 
