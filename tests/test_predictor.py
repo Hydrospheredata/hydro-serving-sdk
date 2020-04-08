8288c748a77476db108555d1304e94b6bdf4423c
techkuz <k.leshakov@yandex.ru>
2020-04-08 15:42:58 +0300

    Add predictor tests

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -1,48 +1,169 @@
 import time
 
-from hydro_serving_grpc.tf import TensorProto, TensorShapeProto, DT_DOUBLE
-from hydro_serving_grpc.tf.api.model_pb2 import ModelSpec
+import numpy as np
+from hydro_serving_grpc.contract import ModelContract
+from pandas import DataFrame
 
-from hydrosdk.predictor import PredictServiceClient
-from tests.test_application import create_test_application
-from tests.test_model import get_cluster
+from hydrosdk.servable import Servable
+from tests.test_model import get_cluster, get_local_model, get_signature
 
 
-# TODO: Fix test after re-struct
-from tests.test_servable import create_test_servable
+# TODO: Add more valid assert
+def test_predict_list():
+    # test sending values in list [-1,2]
+
+    grpc_cluster = get_cluster("0.0.0.0:9090")
+    http_cluster = get_cluster()
+
+    signature = get_signature()
+
+    contract = ModelContract(predict=signature)
+
+    model = get_local_model(contract=contract)
+
+    upload_resp = model.upload(http_cluster)
+
+    # wait for model to upload
+    time.sleep(10)
+
+    created_servable = Servable.create(model_name=upload_resp[model].model.name,
+                                       model_version=upload_resp[model].model.version, cluster=http_cluster,
+                                       grpc_cluster=grpc_cluster)
+
+    # wait for servable to assemble
+    time.sleep(20)
+
+    predictor_client = created_servable.predictor()
+
+    inputs = {'in1': [-1, 2]}
+    predictions = predictor_client.predict(inputs=inputs)
+
+    assert isinstance(predictions['in1'], list)
+    assert isinstance(predictions, dict)
+
+
+def test_predict_pythontype():
+    # test sending values in python type -1
+
+    grpc_cluster = get_cluster("0.0.0.0:9090")
+    http_cluster = get_cluster()
+
+    signature = get_signature()
+
+    contract = ModelContract(predict=signature)
+
+    model = get_local_model(contract=contract)
+
+    upload_resp = model.upload(http_cluster)
+
+    # wait for model to upload
+    time.sleep(10)
+
+    created_servable = Servable.create(model_name=upload_resp[model].model.name,
+                                       model_version=upload_resp[model].model.version, cluster=http_cluster,
+                                       grpc_cluster=grpc_cluster)
+
+    # wait for servable to assemble
+    time.sleep(20)
+
+    predictor_client = created_servable.predictor()
+    inputs = {'in1': -1}
+    predictions = predictor_client.predict(inputs=inputs)
+
+    assert isinstance(predictions, dict)
+
+
+def test_predict_nparray():
+    # test sending values in np.ndarray(1,2,3,4)
+
+    grpc_cluster = get_cluster("0.0.0.0:9090")
+    http_cluster = get_cluster()
+
+    signature = get_signature()
+
+    contract = ModelContract(predict=signature)
+
+    model = get_local_model(contract=contract)
+
+    upload_resp = model.upload(http_cluster)
+
+    # wait for model to upload
+    time.sleep(10)
+
+    created_servable = Servable.create(model_name=upload_resp[model].model.name,
+                                       model_version=upload_resp[model].model.version, cluster=http_cluster,
+                                       grpc_cluster=grpc_cluster)
+
+    # wait for servable to assemble
+    time.sleep(20)
+
+    predictor_client = created_servable.predictor()
+    inputs = {'in1': np.array([1])}
+    predictions = predictor_client.predict(inputs=inputs)
+
+    assert isinstance(predictions['in1'], np.array)
+    assert isinstance(predictions, dict)
+
+
+def test_predict_df():
+    # test sending values in df({"in1":[1,1]})
+
+    grpc_cluster = get_cluster("0.0.0.0:9090")
+    http_cluster = get_cluster()
+
+    signature = get_signature()
+
+    contract = ModelContract(predict=signature)
+
+    model = get_local_model(contract=contract)
+
+    upload_resp = model.upload(http_cluster)
+
+    # wait for model to upload
+    time.sleep(10)
+
+    created_servable = Servable.create(model_name=upload_resp[model].model.name,
+                                       model_version=upload_resp[model].model.version, cluster=http_cluster,
+                                       grpc_cluster=grpc_cluster)
+
+    # wait for servable to assemble
+    time.sleep(20)
+
+    predictor_client = created_servable.predictor()
+    inputs_dict = {'in1': [1]}
+    inputs_df = DataFrame(inputs_dict)
+    predictions = predictor_client.predict(inputs=inputs_df)
+
+    assert isinstance(predictions, DataFrame)
+
+# TODO: do we need series?
+# def test_predict_series():
+#     # test sending values in df({"in1":[1,1]})
 #
-#
-# def test_predict():
 #     grpc_cluster = get_cluster("0.0.0.0:9090")
+#     http_cluster = get_cluster()
 #
-#     created_servable = create_test_servable()
+#     signature = get_signature()
 #
-#     model_spec = ModelSpec(name=created_servable.name)
+#     contract = ModelContract(predict=signature)
 #
-#     # wait for servable to assemble
+#     model = get_local_model(contract=contract)
+#
+#     upload_resp = model.upload(http_cluster)
+#
+#     # wait for model to upload
 #     time.sleep(10)
 #
-#     dtype = DT_DOUBLE
+#     created_servable = Servable.create(model_name=upload_resp[model].model.name,
+#                                        model_version=upload_resp[model].model.version, cluster=http_cluster,
+#                                        grpc_cluster=grpc_cluster)
 #
-#     dim_one = TensorShapeProto.Dim(size=-1)
-#     dim_two = TensorShapeProto.Dim(size=2)
-#     tensor_shape = TensorShapeProto(dim=[dim_one, dim_two])
-#     tensor = TensorProto(dtype=dtype, tensor_shape=tensor_shape)
-#     to_pass = {'in1': tensor}
+#     # wait for servable to assemble
+#     time.sleep(20)
 #
-#     psc = PredictServiceClient(cluster=grpc_cluster)
-#     result = psc.predict(inputs=to_pass, model_spec=model_spec)
+#     predictor_client = created_servable.predictor()
+#     inputs_dict = {'in1': [1]}
+#     inputs_df = Series(inputs_dict)
+#     predictions = predictor_client.predict(inputs=inputs_df)
 #
-#     assert result
-
-
-def test_servable_preditor_create():
-    created_servable = create_test_servable()
-    assert isinstance(created_servable.predictor(monitoring=True, ssl=True), PredictServiceClient)
-
-
-def test_application_predictor_create():
-    cluster = get_cluster()
-
-    created_application = create_test_application(cluster=cluster)
-    assert isinstance(created_application.predictor(monitoring=True, ssl=True), PredictServiceClient)
+#     assert isinstance(predictions, DataFrame)
