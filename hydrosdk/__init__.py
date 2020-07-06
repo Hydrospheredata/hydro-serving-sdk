18c74ef2e6a91849f78c9787b1d72203045664b1
Yuriy <snyderino@gmail.com>
2020-07-06 14:32:22 +0300

    Add major entities to init scope (#65)
    Expose the most frequently used entities in an __init__.py

--- hydrosdk/__init__.py
+++ hydrosdk/__init__.py
@@ -1,1 +1,11 @@
-from hydrosdk.data.types import scalar
+import importlib_metadata
+
+from hydrosdk.modelversion import LocalModel, ModelVersion
+from hydrosdk.image import DockerImage
+from hydrosdk.application import Application
+from hydrosdk.cluster import Cluster
+from hydrosdk.contract import SignatureBuilder, ModelContract
+from hydrosdk.monitoring import MetricSpec, MetricSpecConfig, TresholdCmpOp
+
+
+__version__ = importlib_metadata.version("hydrosdk")
