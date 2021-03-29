6a682a262cf393d411c81a39f2c3ae253030e73b
Ilnur Garifullin <ilnur.email@gmail.com>
2021-03-29 14:50:42 +0300

    Refine tests configuration file

--- tests/config.py
+++ tests/config.py
@@ -13,58 +13,7 @@ config = {
             "url": "hydro-grpc.dev.hydrosphere.io:443",
             "ssl": True
         },
-    "test_model":
-        {
-            "path": "tests/resources/identity_model",
-            "serving": "serving.yaml"
-        },
-    "api_endpoint": "api/v2",
-    "endpoint":
-        {"metric_spec": {
-            "list": "/monitoring/metricspec",
-            "create": "/monitoring/metricspec",
-            "get": "/monitoring/metricspec/{specId}",
-            "delete": "/monitoring/metricspec/{specId}",
-            "get_for_model_version": "/monitoring/metricspec/modelversion/{versionId}"
-        },
-            "application": {
-                "delete": "/application/",
-                "generate_input": "/application/generateInputs/",
-                "get": "/application/{appName}",
-                "applications": "/application",
-                "add": "/application",
-                "update": "/application"
-            },
-            "host_selector": {
-                "list": "/hostSelector",
-                "create": "/hostSelector",
-                "get": "/hostSelector/{envName}",
-                "delete": "/hostSelector/{envName}"
-            },
-
-            "external_model": {
-                "register": "/extrenalmodel"
-            },
-
-            "model": {
-                "list": "/model",
-                "all_version": "/model/version",
-                "get_version": "/model/version/{versionName}/{version}",
-                "get": "/model/{modelId}",
-                "delete": "/model/{modelId}",
-                "upload": "/model/upload"
-            },
-
-            "servable":
-                {
-                    "servables": "/servable",
-                    "deploy": "/servable",
-                    "get": "/servable/{name}",
-                    "stop": "/servable/{name}",
-                    "get_logs": "/servable/{name}/logs"
-                }
-
-        },
+    "lock_timeout": 120,
     "default_application_name": "infer",
     "default_model_name": "infer"
 }
@@ -77,5 +26,5 @@ DEFAULT_APP_NAME = config["default_application_name"]
 DEFAULT_MODEL_NAME = config["default_model_name"]
 DEFAULT_RUNTIME_IMAGE = config["runtime"]["image"]
 DEFAULT_RUNTIME_TAG = config["runtime"]["tag"]
-
-# TODO: add error messages to all tests, hard to debug otherwise
+DEFAULT_RUNTIME_REFERENCE = f"{DEFAULT_RUNTIME_IMAGE}:{DEFAULT_RUNTIME_TAG}"
+LOCK_TIMEOUT = config["lock_timeout"]
